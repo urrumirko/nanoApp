516ddcee9fceedd17b49d6619aedf284fd2f1bda
urrumirko <mirkourru@MBP-di-Mirko.station>
2018-08-16 21:58:34 +0200

    Update Readme

--- frontend-app/e2e/app.po.ts
+++ frontend-app/e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-export class TutorialAngularStylingClarityPage {
+export class NanoDashboardPage {
   navigateTo() {
     return browser.get('/');
   }
